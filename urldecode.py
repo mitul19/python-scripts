b08e0e674d01697081f1c3ba954177f57ec2b331
Mitul Patel <mpatel@akamai.com>
2021-06-28 11:19:36 +0530

    new files

---
+++ urldecode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import urllib.parse as ul
+
+'''
+urlencode Program.
+Auther : Mitul Patel 
+github : https://github.com/mitul19
+Open Source Code.
+'''
+
+# take argument and print encoded output
+print(ul.unquote_plus(sys.argv[1]))
