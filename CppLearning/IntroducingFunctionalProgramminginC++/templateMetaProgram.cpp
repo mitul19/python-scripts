1a1e56eea79e732c4d176ef26a6439a675724941
Mitul Patel <patelmitul87@gmail.com>
2023-06-22 15:01:18 +0530

    functionaProgramingpush2

---
+++ CppLearning/IntroducingFunctionalProgramminginC++/templateMetaProgram.cpp
@@ -0,0 +1,7 @@
+/*
+Template Metaprogramming or TMP is a form of C++ programming. 
+TMP is a Turing complete, functional programming language built-in to C++. 
+TMP variables are always immutable. 
+TMP doesn't have loops, so we must use recursion. 
+TMP doesn't have branching, so we use specialization.
+*/
